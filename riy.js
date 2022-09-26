828bbc8b9ace79ead96a7ba817a4535d6a566053
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2022-09-26 16:17:05 +0700

    Update riy.js

--- riy.js
+++ riy.js
@@ -1,6 +1,6 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
+› Create By @PET
+› Base Ori @Petbase
 › Kalau mau ubah/edit owner di settings.js
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
