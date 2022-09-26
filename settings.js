1dfc5a90c111e05639c17d8a148294fbc8507a05
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2022-09-26 14:44:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
+› Create By @Pet
+› Base Ori @Petdatabase
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
 */
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281280174445'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281280174445'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281280174445' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Petz' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Peter-Botz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Pet' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.youtube = 'https://youtube.com/channel/UC2yce20yVSvDQLI3uL5wUjw'
+global.ig = 'https://instagram.com/onlypet_oky'
+global.mygc = 'https://chat.whatsapp.com/JEv6rKBV6ZwE923iMZW9R5'
+global.myweb = 'https://github.com/Peterelzevir/Peter-Botz/'
+global.email = 'Peterbotz@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Peter-Botz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Pet' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
