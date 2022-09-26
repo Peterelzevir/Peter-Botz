3d2691b3b0b638ae608ee618d4d4d2a432833108
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2022-09-26 14:37:06 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
+› Create By @PET
+› Base Ori @Petdata
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
 */
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281280174445'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281280174445'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281280174445' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Pet' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Pet-Botz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Pet' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.youtube = 'https://youtube.com/channel/UC2yce20yVSvDQLI3uL5wUjw'
+global.ig = 'https://youtube.com/channel/UC2yce20yVSvDQLI3uL5wUjw'
+global.mygc = 'https://chat.whatsapp.com/JEv6rKBV6ZwE923iMZW9R5'
+global.myweb = 'https://instagram.com/onlypet_oky'
+global.email = 'Peterbotz@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Pet-Botz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Pet' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -83,15 +83,15 @@ global.sp = '⭔'
 //terserah mau ubah apa nggak, note : tanda ' gausah di hapus!
 global.mess = {
 success: '_Done._',
-admin: '_Fitur Khusus Admin Group!_',
-botAdmin: '_Bot Harus Menjadi Admin Terlebih Dahulu!_',
-owner: '_Fitur Khusus Owner Bot_',
-group: '_Fitur Digunakan Hanya Untuk Group!_',
+admin: '_Fitur Khusus Admin Group Cuy!_',
+botAdmin: '_Bot Harus Menjadi Admin Dahulu Ya Guys!_',
+owner: '_Fitur Khusus Owner Petz Botz_',
+group: '_Fitur Khusus Hanya Untuk Group!_',
 private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
 bot: '_Fitur Khusus Pengguna Nomor Bot_',
-wait: '_Sedang Di Proses..._',
-error: '_Fitur Sedang Error!_',
-endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
+wait: '_Proses..._',
+error: '_Fitur Lagi Error!_',
+endLimit: '_Limit Harian Kamu Telah Habis, Limit Selalu Direset Setiap Jam 12_',
 }
 
 //—————「 Batas Akhir 」—————//
